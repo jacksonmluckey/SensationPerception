13faf0b2a9af656610d40e9cddf09176536f1ef1
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-11-05 10:05:11 -0800

    commented out the code that I was implementing from stack overflow to handle the forms within django because time constraints demand google forms

--- experiment/models.py
+++ experiment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from experiment.choices import LIKERT_SCALE
+'''
+from experiment.likert import LIKERT_SCALE
 
 class Smartphone(models.Model):
     on_you_freq = models.IntegerField(choices=LIKERT_SCALE, default=1)
+'''
