995e93094b4a0ba516e89876736dcfa100831607
Jackson M Luckey <jacksonmluckey@gmail.com>
2019-11-05 09:18:13 -0800

    added a test model to models.py to test out the likert scale

--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from experiment.choices import LIKERT_SCALE
 
-# Create your models here.
+class Smartphone(models.Model):
+    on_you_freq = models.IntegerField(choices=LIKERT_SCALE, default=1)
